8c3e919a1aef8f038a4c460f7885510c1252a209
jaap aarts <jaap.aarts1@gmail.com>
2020-07-28 13:37:35 +0200

    made test files be directory based

--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
@@ -22,42 +24,27 @@ func TestEverything(t *testing.T) {
 }
 
 func testWRT(t *testing.T, name string) {
+	defer os.RemoveAll("etc")
 	genesisCommit("tests/" + name + ".toml")
-	out, err := ioutil.ReadFile("./etc/config/network")
-	if err != nil {
-		t.Fatal("failled to open network output file")
-	}
-	res, err := ioutil.ReadFile("tests/" + name + ".network")
-	if err != nil {
-		t.Fatal("no expected network file")
-	}
-	if bytes.Compare(out, res) != 0 {
-		t.Error("network file not the same")
-	}
-	out, err = ioutil.ReadFile("./etc/config/wireless")
-	if err != nil {
-		t.Fatal("failled to open wireless output file")
-	}
-	res, err = ioutil.ReadFile("tests/" + name + ".wireless")
-	if err != nil {
-		t.Fatal("no expected wireless file")
-	}
-	if bytes.Compare(out, res) != 0 {
-		t.Error("wireless file not the same")
-	}
+	filepath.Walk("etc", VisitFile(t, name))
 }
 
 func testSystemd(t *testing.T, name string) {
+	defer os.RemoveAll("etc")
 	genesisCommit("tests/" + name + ".toml")
-	files, _ := ioutil.ReadDir("tests")
-	for _, file := range files {
-		if !strings.HasPrefix(file.Name(), name+".d.") {
-			continue
+	filepath.Walk("etc", VisitFile(t, name))
+}
+
+func VisitFile(t *testing.T, name string) func(fp string, fi os.FileInfo, err error) error {
+	return func(fp string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-		of := "./etc/systemd/network/20-genesis-" + strings.TrimPrefix(file.Name(), name+".d.")
-		if !fileEQ(t, "tests/"+file.Name(), of) {
-			t.Fail()
+		if fi.IsDir() {
+			return nil
 		}
+		fileEQ(t, filepath.Join("tests", name, fp), fp)
+		return nil
 	}
 }
 
