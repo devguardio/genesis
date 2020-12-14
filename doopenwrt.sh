957918f2b47ed1edddb45dc03b7ccdf077b6c45e
Arvid E. Picciani <aep@exys.org>
2020-12-14 19:42:21 +0100

    m

--- doopenwrt.sh
+++ doopenwrt.sh
@@ -1,7 +1,14 @@
 set -ex
-zz build
+zz build --release
+
+
+rm -rf ~/proj/captif/openwrt/package/devguard/genesis/src/
+mkdir -p ~/proj/captif/openwrt/package/devguard/genesis/src/
+
+cp -a target/gen target/c/ target/make/ target/release ~/proj/captif/openwrt/package/devguard/genesis/src/
+
 
 cd ~/proj/captif/openwrt/
-make V=s -j20 package/devguard/genesis/{clean,compile}
-scp ~/proj/captif/openwrt/build_dir/target-mips_24kc_musl/genesis-0.13/genesis  root@192.168.0.83:/tmp/genesis
+make V=s CONFIG_DEBUG=y -j20 package/devguard/genesis/{clean,compile}
+scp ~/proj/captif/openwrt/build_dir/target-mips_24kc_musl/genesis-0.13/genesis root@192.168.0.83:/tmp/genesis
 
